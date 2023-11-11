faf4c95562dab005b482a04f8f8a6459a0069b41
Gonar21 <ale_roger1234@hotmail.es>
2023-11-10 23:22:20 -0600

    cambios otra vez

--- App/Views/PanelUsuario_View.php
+++ App/Views/PanelUsuario_View.php
@@ -4,45 +4,52 @@ include './templates/Header.php';
 <!-- ==============================================Fin header ======= -->
 <main id="main">
 <link href="../assets/css/panelHuesped.css" rel="stylesheet">
-<head>
-    
-</head>
 <body>
-    <div class="cards-container">
-        <div class="card">
-            <a href="mi_banco.php">Mi Banco</a>
+    <br>
+        <div class="cards-container">
+            <div class="card">
+                <a href="mi_banco.php">Mi Banco</a>
+            </div>
+            <div class="card">
+                <a href="perfil.php">Datos Personales</a>
+            </div>
+            <div class="card">
+                <a href="validar_identidad.php">Validar Identidad</a>
+            </div>
+            <div class="card">
+                <a href="notificaciones.php">Notificaciones</a>
+            </div>
+            <div class="card">
+                <a href="reservas.php">Historial de Reservas</a>
+            </div>
         </div>
-        <div class="card">
-            <a href="perfil.php">Datos Personales</a>
+            </div>
+                <div class="image-container">
+                <img src="../assets/img/logo/logo2.png" alt="Imagen Central">
+            </div>
+        <div class="cards-container">
+            <div class="card">
+                <a href="admin_seguridad.php">Administración de Seguridad</a>
+            </div>
+            <div class="card">
+                <a href="cupones_tarjetas.php">Cupones y Tarjetas de Regalo</a>
+            </div>
+            <div class="card">
+                <a href="denuncias.php">Denuncias</a>
+            </div>
+            <div class="card">
+                <a href="mensajes.php">Mensajes</a>
+            </div>
+            <div class="card">
+                <a href="mis_resenas.php">Historial de Reseñas</a>
+            </div>
+            
         </div>
-        <div class="card">
-            <a href="validar_identidad.php">Validar Identidad</a>
-        </div>
-        <div class="card">
-            <a href="notificaciones.php">Notificaciones</a>
-        </div>
-        <div class="card">
-            <a href="reservas.php">Historial de Reservas</a>
-        </div>
-        <div class="card">
-            <a href="admin_seguridad.php">Administración de Seguridad</a>
-        </div>
-        <div class="card">
-            <a href="cupones_tarjetas.php">Cupones y Tarjetas de Regalo</a>
-        </div>
-        <div class="card">
-            <a href="denuncias.php">Denuncias</a>
-        </div>
-        <div class="card">
-            <a href="mensajes.php">Mensajes</a>
-        </div>
-        <div class="card">
-            <a href="mis_resenas.php">Historial de Reseñas</a>
-        </div>
-    </div>
+    <br>
 </body>
 
 </main>
+
 <!-- ==============================================Inicio Footer ======= -->
 <?php
 include './templates/Footer.php';
