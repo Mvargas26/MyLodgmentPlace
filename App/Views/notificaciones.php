c1c89a98ff5f392d1df94f65767eb0c18d2854bb
Gonar21 <ale_roger1234@hotmail.es>
2023-11-10 23:37:57 -0600

    muchas paginas jajaja

---
+++ App/Views/notificaciones.php
@@ -0,0 +1,27 @@
+<?php
+include './templates/Header.php';
+?>
+<!-- ==============================================Fin header ======= -->
+<main id="main">
+
+<body id="body">
+
+
+<p>aca va las notificaciones y las reseñas </p>
+
+
+
+
+</body>
+
+
+</main>
+
+
+  
+
+<!-- ==============================================Inicio Footer ======= -->
+<?php
+include './templates/Footer.php';
+?>
+<!-- ==============================================Fin Footer ======= -->
