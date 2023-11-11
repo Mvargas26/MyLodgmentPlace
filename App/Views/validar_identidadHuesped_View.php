fdbbca8a6b50e0e99a20fde9f5488884434e8e6a
Gonar21 <ale_roger1234@hotmail.es>
2023-11-11 17:30:15 -0600

    cambios graficos

--- App/Views/validar_identidadHuesped_View.php
+++ App/Views/validar_identidadHuesped_View.php
@@ -12,7 +12,7 @@ include './templates/Header.php';
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="../assets/css/validarIdentidad.css">
+    <link href="../assets/css/validarIdentidad.css" rel="stylesheet">
 
 </head>
 <body>
@@ -29,7 +29,7 @@ include './templates/Header.php';
         <!-- Botón para abrir el modal de subir foto -->
         <br>
         <br>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#uploadModal">
+        <button type="button" id="uploadButton" class="btn btn-primary" data-toggle="modal" data-target="#uploadModal">
             Subir Foto
         </button>
     </div>
@@ -41,7 +41,7 @@ include './templates/Header.php';
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="uploadModalLabel">Subir Foto</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close" class="btn btn-primary" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
                 <br>
@@ -51,11 +51,11 @@ include './templates/Header.php';
                 <div id="uploadContainer">
                     <p>Haz clic para subir una foto</p>
                     <input type="file" id="fileInput" style="display: none">
-                    <label for="fileInput" id="uploadButton" class="btn btn-primary">Seleccionar Foto</label>
+                    <label for="fileInput" class="btn btn-primary" id="uploadButton">Seleccionar Foto</label>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" onclick="guardarDatos()">Guardar Datos</button>
+                <button type="button" id="uploadButton" class="btn btn-primary" onclick="guardarDatos()">Guardar Datos</button>
             </div>
         </div>
     </div>
