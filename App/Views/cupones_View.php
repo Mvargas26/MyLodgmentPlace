9938ced0e4ad002e9393c5a7764ede207bc12cc8
Michael Vargas S <michaelvargas47@hotmail.com>
2023-11-21 16:54:00 -0600

    Revert "asd"
    This reverts commit ecc5c3ba0a27d5b1e2f49d67bab1b0ad8ccff47d.

--- App/Views/cupones_View.php
+++ App/Views/cupones_View.php
@@ -1,10 +1,10 @@
 <?php
 include './templates/Header.php';
 session_start();
-// if (!isset($_SESSION['Identificacion']) || !isset($_SESSION['Rol']) || empty($_SESSION['Identificacion']) || empty($_SESSION['Rol'])|| $_SESSION['Rol']!=3) {
-//     header('Location: ../../');
-//     exit();
-// }
+if (!isset($_SESSION['Identificacion']) || !isset($_SESSION['Rol']) || empty($_SESSION['Identificacion']) || empty($_SESSION['Rol'])|| $_SESSION['Rol']!=2) {
+    header('Location: ../../');
+    exit();
+}
 ?>
 <!-- ==============================================Fin header ======= -->
 <main id="main">
