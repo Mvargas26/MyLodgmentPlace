553c4797d83be57e0475c31cdcf5cd3e6ea566b2
Michael Vargas S <michaelvargas47@hotmail.com>
2023-12-08 23:00:55 -0600

    registra la reserva

--- App/Modules/ReservaLugar/ReservaLugar_Negocios.php
+++ App/Modules/ReservaLugar/ReservaLugar_Negocios.php
@@ -19,48 +19,21 @@
                 $usuario = intval($idUsuario);
                 $propietario = intval($idPropietario);
                 $monto = floatval($valorTotal);
-                
+
+                //rebaja la plata
                 $resultado = consumirEndpointPOST($propietario, $usuario, $monto);
 
+                //Inserta la reserva
+                $resultadoInsert = $ObjMaster->InsertarReserva($idUsuario, $idInmueble,$fechaInicio, $fechaFin, $valorTotal, $valorTotalImpuestos, $cantidadPersonasExtra, $cantidadPersonas);
+
+
+
                 if ($resultado) {
-                    echo json_encode(array('exito' => true, 'mensaje' => $resultado));
+                    echo json_encode(array('exito' => true, 'mensajeAPI' => $resultado,'mensajeReserva' => $resultadoInsert));
                 } else {
-                    echo json_encode(array('error' => true, 'mensaje' => $resultado));
+                    echo json_encode(array('error' => true, 'mensajeAPI' => $resultado,'mensajeReserva' => $resultadoInsert));
                 }
-        
 
-                
-                
-                //     if (isset($_POST['data'])) {
-
-
-    //         // $ReservaCalificar = $_POST['formData']['ReservaCalificar'];
-    //         // $comentarioCalificacion = $_POST['formData']['comentarioCalificacion'];
-    //         // $estrellasSeleccionadas = $_POST['formData']['estrellasSeleccionadas'];
-    //         // $cedAnfitrionCalifica = $_POST['formData']['cedAnfitrionCalifica'];
-    //         // //calificar Huesped es 1
-    //         // if ($_POST['formData']['Rol']==2) {
-    //         //     $tipoCalificacion = 1;
-    //         // }else{
-    //         //     $tipoCalificacion = 3;
-    //         // }
-            
-    //         //  $resultadoConsulta = $ObjMaster->InsertarCalificacion( $ReservaCalificar, $comentarioCalificacion,
-    //         //  $estrellasSeleccionadas,$cedAnfitrionCalifica,$tipoCalificacion);
-
-    //         //  $notificacion_Huesped_y_Anfitrion = $ObjMaster->InsertarNotificacion_CuandoCalificaElAnfitrion($cedAnfitrionCalifica , $ReservaCalificar);
-             
-    //         //  if ($resultadoConsulta && $notificacion_Huesped_y_Anfitrion) {
-
-                
-    //         //     echo json_encode(array('exito' => true));
-    //         // } else {
-    //         //     echo json_encode(array('exito' => false));
-    //         // }
-    //     }else{
-    //        echo json_encode(array('Vacio' =>'No entro al If'));
-    //     }
-        
     } catch (Exception $e) {
         echo json_encode(array('error' => $e->getMessage()));
     }
