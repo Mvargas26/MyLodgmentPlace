7f2d6c6e39d8e4d33cefcf09c549a2e08e731686
Michael Vargas S. <michaelvargas47@hotmail.com>
2024-08-20 22:26:03 -0600

    Login Nuevo

--- App/Views/Login_View.php
+++ App/Views/Login_View.php
@@ -1,26 +1,40 @@
 <?php include './templates/Header.php'; ?>
 
+<link href="<?php echo $base_path; ?>css/loginNuevo.css" rel="stylesheet">
+
 <main id="main">
+    <body>
+       <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <!-- Design by foolishdeveloper.com -->
+            <title>Login My Lodgment Place</title>
 
-    <body id="bodyLogin">
-        <div class="login-reg-panel">
-            <div id="divImagen">
-                <img id="imgLogo" src="../assets/img/logo/logo2.png" alt="">
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+   
+        </head>
+        <body>
+            <div class="background">
+                <div class="shape"></div>
+                <div class="shape"></div>
             </div>
-            <!-- Resto de tu código HTML -->
+            <form action="" method="post" id="loginForm">
+                <img src="<?php echo $base_path; ?>img/logo/Logo_blanco2.png" alt="Logo" class="form-logo">
+                <h6>Ingresa tus credenciales:</h6>
 
-            <div class="white-panel">
-                <div class="login-show">
-                    <h2>Inicio de Sesion</h2>
-                    <!-- Agrega un formulario de login -->
-                    <form action="" method="post" id="loginForm">
-                        <input type="number" name="identificacion" placeholder="Identificación" required>
-                        <input type="password" name="password" placeholder="Contraseña" required>
-                        <input type="submit" value="Entrar">
-                    </form>
-                </div>
-            </div>
-        </div>
+                <label for="identificacion">identificación</label>
+                <input type="number" name="identificacion" placeholder="Identificación" required>
+
+                <label for="password">Password</label>
+                <input type="password" name="password" placeholder="Contraseña" required>
+
+                <input type="submit" value="Entrar" class="btnLogin">
+            </form>
+        </body>
+
+        </html>
     </body>
 </main>
 <script>
@@ -30,4 +44,22 @@
 </script>
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
-<?php include './templates/Footer.php'; ?>
+<!-- Vendor JS Files -->
+<script src="../assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="../assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="../assets/js/main.js"></script>
+<script src="../assets/js/script.js"></script>
+<script src="../assets/js/script_personalizado.js"></script>
+<script src="../assets/js/script_validarIdentidad.js"></script>
+<script src="../assets/js/VistaDetalle/scriptVistaDetalle.js"></script>
+<script src="../assets/js/Registro/registroScript.js"></script>
+<script src="../assets/js/loginNuevo.js"></script>
+</body>
+
+</html>
